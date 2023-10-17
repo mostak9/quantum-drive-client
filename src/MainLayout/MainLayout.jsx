6d3d4de9bff9e8a30b21bceec04e704b407fb59b
mostak9 <mostak.ahm263@gmail.com>
2023-10-17 20:51:22 +0600

    setup client side working enviroment and added logo, fonts, router

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-poppins font-bold">This is main layout</h2>
+        </div>
+    );
+};
+
+export default MainLayout;
